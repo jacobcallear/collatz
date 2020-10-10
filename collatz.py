23be31b37870a9114400e6a3eecdd75b54991e97
Jacob Callear <jacob18@callear.net>
2020-10-10 15:10:39 +0100

    Add sequence methods to class

--- collatz.py
+++ collatz.py
@@ -13,7 +13,7 @@ class Collatz:
             raise ValueError('First term must be a whole number')
         if start_number < 1:
             raise ValueError('First term must be > 0')
-        self.sequence = tuple(self._generate_sequence(start_number))
+        self._sequence = tuple(self._generate_sequence(start_number))
     
     @staticmethod
     def _generate_sequence(num):
@@ -27,11 +27,25 @@ class Collatz:
             yield num
 
     def __repr__(self):
-        first_term = self.sequence[0]
-        length = len(self.sequence)
+        first_term = self._sequence[0]
+        length = len(self._sequence)
         return f'Collatz(first_term={first_term:,}, length={length})'
 
     def __str__(self):
-        first_term = self.sequence[0]
-        length = len(self.sequence)
+        first_term = self._sequence[0]
+        length = len(self._sequence)
         return f'Collatz sequence starting from {first_term:,} with {length} terms'
+
+    def __iter__(self):
+        return iter(self._sequence)
+    
+    def __getitem__(self, item):
+        return self._sequence[item]
+
+    def __eq__(self, other):
+        if self._sequence == other:
+            return True
+        return False
+    
+    def __len__(self):
+        return len(self._sequence)
