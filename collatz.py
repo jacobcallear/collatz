6f661044af49f2e99320d22f43d83ae67e38a0e9
Jacob Callear <jacob18@callear.net>
2020-10-14 18:51:39 +0100

    Improve repr and add scientific form options

--- collatz.py
+++ collatz.py
@@ -3,7 +3,7 @@
 from functools import total_ordering
 
 __version__ = '0.0'
-__author = 'Jacob'
+__author__ = 'Jacob'
 
 @total_ordering
 class Collatz:
@@ -31,14 +31,28 @@ class Collatz:
             yield num
 
     def __repr__(self):
-        first_term = self.convert_to_scientific_form(self._sequence[0])
-        length = len(self._sequence)
-        return f'Collatz(first_term={first_term}, length={length})'
+        '''Returns numpy.array style repr.'''
+        def sequence_to_string(sequence):
+            scientific_form_sequence = (
+                self.convert_to_scientific_form(term, decimal_places=3, threshold=10)
+                for term in sequence
+            )
+            return ', '.join(scientific_form_sequence)
+        # ----------
+        if len(self._sequence) < 10:
+            abbreviated_sequence = str(self._sequence).strip('()')
+        else:
+            first_three_terms = sequence_to_string(self._sequence[:3])
+            last_five_terms = sequence_to_string(self._sequence[-5:])
+            abbreviated_sequence = f'{first_three_terms}, ..., {last_five_terms}'
+        return f'{type(self).__name__}([{abbreviated_sequence}])'
 
     def __str__(self):
-        first_term = self._sequence[0]
-        length = len(self._sequence)
-        return f'Collatz sequence starting from {first_term:,} with {length} terms'
+        first_term = self.convert_to_scientific_form(self._sequence[0],
+                                                     decimal_places=2)
+        length = self.convert_to_scientific_form(len(self._sequence),
+                                                     decimal_places=2)
+        return f'Collatz sequence starting from {first_term} with {length} terms'
 
     def __iter__(self):
         return iter(self._sequence)
@@ -61,20 +75,32 @@ class Collatz:
 
     # ==============================
     @staticmethod
-    def convert_to_scientific_form(number):
-        '''Convert any positive integer to scientific form (6 decimal places).
+    def convert_to_scientific_form(number, decimal_places=6, threshold=10):
+        '''Convert a positive integer to scientific form or add comma separators.
         
         Doesn't raise OverflowError for large numbers, unlike f'{:e}'
+
+        Args:
+            number (int): Number to format
+            decimal_places (int): Number of decimal places for scientific form.
+                Must be < 15.
+            threshold (int): Numbers with more digits than the threshold are
+                converted to scientific form. Otherwise, commas separators are
+                added.
         '''
+        if decimal_places > 14:
+            raise ValueError('Maximum precision 14 decimal places')
+        if threshold is None or len(str(number)) <= threshold:
+            return f'{number:,}'
         # If all digits after decimal point == 0, set precision to exact
-        if all(ch == '0' for ch in str(number)[7:]):
+        if all(ch == '0' for ch in str(number)[decimal_places+1:]):
             precision = ''
         else:
             precision = '~'
         exponent = len(str(number)) - 1
-        coefficient = f'{number / 10**exponent:.6f}'
+        coefficient = f'{number / 10**exponent:.{decimal_places}f}'
         # If rounding resulted in a coefficient of 10...
         if coefficient.startswith('10.'):
             coefficient = coefficient.replace('10.', '1.', 1)
             exponent += 1
-        return f'{precision}{coefficient} * 10**{exponent}'
+        return f'{precision}{coefficient} x 10^{exponent}'
