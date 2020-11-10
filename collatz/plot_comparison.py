0b9989c54e1b12c110e9e4ed761b7e9e830565c9
Jacob Callear <jacob18@callear.net>
2020-11-10 10:10:44 +0000

    Add legend to plot comparison

--- collatz/plot_comparison.py
+++ collatz/plot_comparison.py
@@ -2,15 +2,19 @@
 '''
 from matplotlib import pyplot as plt
 
+from collatz import Collatz
+
 def plot_comparison(sequences, scale='linear'):
-    '''Plot each sequence on the same plot.'''
+    '''Plot each sequence in sequences on the same plot.'''
     linear = True if scale == 'linear' else False
     plt.style.use('ggplot')
     for sequence in sequences:
         if len(sequence) == 1:
             print(f'ERROR: Sequence {sequence!r} only contains one element')
+        x_values = range(len(sequence))
+        sequence_label = Collatz.convert_to_scientific_form(sequence[0])
         try:
-            plt.plot(range(len(sequence)), sequence)
+            plt.plot(x_values, sequence, label=sequence_label)
         except OverflowError:
             print(f'ERROR: Sequence {sequence!r} too large to plot')
             continue
@@ -25,4 +29,6 @@ def plot_comparison(sequences, scale='linear'):
     # Scale
     scale = 'linear' if linear else 'log'
     plt.yscale(scale)
+    if len(sequences) <= 10:
+        plt.legend(title='First term', facecolor='#fdf6e3', framealpha=1)
     plt.show()
