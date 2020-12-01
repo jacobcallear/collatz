f120e8b61fe087367b77c264e301ebaccb6963ff
Jacob Callear <jacob18@callear.net>
2020-12-01 13:07:40 +0000

    Extend tests for sequence generation

--- tests/test_collatz.py
+++ tests/test_collatz.py
@@ -2,31 +2,25 @@
 import pytest
 
 from collatz import Collatz
-
-@pytest.fixture
-def example_sequence():
-    return (
-    27, 82, 41, 124, 62, 31, 94, 47, 142, 71, 214, 107, 322, 161, 484, 242, 121,
-    364, 182, 91, 274, 137, 412, 206, 103, 310, 155, 466, 233, 700, 350, 175,
-    526, 263, 790, 395, 1186, 593, 1780, 890, 445, 1336, 668, 334, 167, 502,
-    251, 754, 377, 1132, 566, 283, 850, 425, 1276, 638, 319, 958, 479, 1438,
-    719, 2158, 1079, 3238, 1619, 4858, 2429, 7288, 3644, 1822, 911, 2734, 1367,
-    4102, 2051, 6154, 3077, 9232, 4616, 2308, 1154, 577, 1732, 866, 433, 1300,
-    650, 325, 976, 488, 244, 122, 61, 184, 92, 46, 23, 70, 35, 106, 53, 160, 80,
-    40, 20, 10, 5, 16, 8, 4, 2, 1
+from tests.example_sequences import (
+    example_sequence_1, example_sequence_2, example_sequence_3,
+    example_sequence_4, example_sequence_5, example_sequence_6,
+    example_sequence_7, example_sequence_8, example_sequence_9,
+    example_sequence_10, example_sequence_27, example_sequence_97,
+    example_sequence_77031, example_sequence_1048576
 )
 
-@pytest.fixture
-def example_collatz_class():
-    return Collatz(27)
-
-def test_setup(example_sequence, example_collatz_class):
-    '''Check that example sequence and collatz class are comparable'''
-    assert example_sequence[0] == example_collatz_class[0]
-
-def test_sequence(example_sequence, example_collatz_class):
-    '''Check Collatz sequence generated correctly.'''
-    assert example_sequence == example_collatz_class
+@pytest.mark.parametrize('example_sequence', [
+    example_sequence_1, example_sequence_2, example_sequence_3,
+    example_sequence_4, example_sequence_5, example_sequence_6,
+    example_sequence_7, example_sequence_8, example_sequence_9,
+    example_sequence_10, example_sequence_27, example_sequence_97,
+    example_sequence_77031, example_sequence_1048576
+])
+def test_generate_sequences(example_sequence):
+    '''Test generated collatz sequences are correct for a few first terms.'''
+    first_term = example_sequence[0]
+    assert Collatz(first_term) == example_sequence
 
 @pytest.mark.parametrize('input_number, decimal_places, threshold, expected_output', [
     (852074902357402935742905784029357842930574, 6, 0, '~8.520749 x 10^41'),
