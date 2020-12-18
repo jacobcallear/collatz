cf97eb7ab58c8cbcf98ad01382302be5585ce075
Jacob Callear <jacob18@callear.net>
2020-12-18 10:06:24 +0000

    Test more inputs with parametrize

--- tests/test_collatz.py
+++ tests/test_collatz.py
@@ -1,4 +1,7 @@
 '''Test Collatz class.'''
+
+import operator
+
 import pytest
 
 from collatz import Collatz
@@ -9,6 +12,7 @@ from tests.example_sequences import (
     example_sequence_10, example_sequence_27, example_sequence_97,
     example_sequence_77031, example_sequence_1048576
 )
+
 
 @pytest.mark.parametrize('example_sequence', [
     example_sequence_1, example_sequence_2, example_sequence_3,
@@ -21,6 +25,7 @@ def test_generate_sequences(example_sequence):
     '''Test generated collatz sequences are correct for a few first terms.'''
     first_term = example_sequence[0]
     assert Collatz(first_term) == example_sequence
+
 
 @pytest.mark.parametrize('input_number, decimal_places, threshold, expected_output', [
     (852074902357402935742905784029357842930574, 6, 0, '~8.520749 x 10^41'),
@@ -38,22 +43,55 @@ def test_convert_to_scientific_form(input_number, decimal_places, threshold,
         threshold=threshold)
     assert expected_output == actual_output
 
-def test_error_raising():
-    '''Check Errors are raised when first term is not an integer > 0.'''
-    for wrong_input_type in ('string', None, [1], (1,), {}):
-        with pytest.raises(TypeError):
-            Collatz(wrong_input_type)
-    with pytest.raises(ValueError):
-        Collatz(1.1)
-    with pytest.raises(ValueError):
-        Collatz(0)
-        Collatz(-1)
 
-def test_ordering():
+@pytest.mark.parametrize('wrong_input_type', [
+    'a string',
+    None,
+    [1],
+    (1,),
+    {},
+    set()
+])
+def test_raise_type_error(wrong_input_type):
+    '''Check TypeError raised when input is not a float or integer.'''
+    with pytest.raises(TypeError):
+        Collatz(wrong_input_type)
+
+
+@pytest.mark.parametrize('wrong_input_value', [
+    1.1,
+    1000.9,
+    0,
+    -0,
+    -1,
+    -1.3,
+    -1000,
+    -1000.5
+])
+def test_raise_value_error(wrong_input_value):
+    '''Check ValueError raised for negative numbers, 0, and non-integer floats.
+    '''
+    with pytest.raises(ValueError):
+        Collatz(wrong_input_value)
+
+
+@pytest.mark.parametrize('number_1, number_2, ordering', [
+    # Collatz(big_num) > Collatz(small_num)
+    (3, 2, operator.gt),
+    (100, 99, operator.gt),
+    (1000, 1, operator.gt),
+    # Collatz(small_num < Collatz(big_num))
+    (1, 4, operator.lt),
+    (149, 150, operator.lt),
+    (3, 1000, operator.lt),
+    # Collatz(x) == Collatz(x)
+    (785, 785, operator.eq),
+    (573, 573, operator.le),
+    (946, 946, operator.ge),
+    # Collatz(x) != Collatz(y)
+    (9, 2, operator.ne),
+    (930457, 3984570, operator.ne)
+])
+def test_ordering(number_1, number_2, ordering):
     '''Test comparison ordering as for tuples.'''
-    assert Collatz(3) > Collatz(2) > Collatz(1)
-    assert Collatz(3) >= Collatz(2) >= Collatz(1)
-    assert Collatz(10) < Collatz(100) < Collatz(1000)
-    assert Collatz(10) <= Collatz(100) <= Collatz(1000)
-    assert Collatz(178) == Collatz(178) != Collatz(177)
-    assert Collatz(203) >= Collatz(203) <= Collatz(203)
+    assert ordering(Collatz(number_1), Collatz(number_2))
